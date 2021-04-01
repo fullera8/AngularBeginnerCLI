b30bbda29beb0b170496812932b3a4710f79e2f7
Alex Fuller <fulleralex47@gmail.com>
2021-04-01 07:42:51 -0400

    Create HTML directive search-box with "ng g d search-box2 --flat false " flat false not required but better structure.

--- angular-hello/src/app/app.module.ts
+++ angular-hello/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
+import { SearchBoxDirective } from './search-box.directive';
+import { SearchBox2Directive } from './search-box2/search-box2.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    OrdersComponent
+    OrdersComponent,
+    SearchBoxDirective,
+    SearchBox2Directive
   ],
   imports: [
     BrowserModule,
