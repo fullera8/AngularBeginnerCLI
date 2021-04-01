5ab2c7d50f101ca2f17e8e772e0a296e29b45430
Alex Fuller <fulleralex47@gmail.com>
2021-04-01 07:36:34 -0400

    New project with components generated with "ng g c" along with options.

---
+++ angular-hello/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
