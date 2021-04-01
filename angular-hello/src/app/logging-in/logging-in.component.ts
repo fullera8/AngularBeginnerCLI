b5034a041a97b4828ca942e095cbe6b475255ad4
Alex Fuller <fulleralex47@gmail.com>
2021-04-01 08:32:23 -0400

    Create new module and component for that module.

---
+++ angular-hello/src/app/logging-in/logging-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logging-in',
+  templateUrl: './logging-in.component.html',
+  styleUrls: ['./logging-in.component.css']
+})
+export class LoggingInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-hello/src/app/login/login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoggingInComponent } from '../logging-in/logging-in.component';
+
+
+
+@NgModule({
+  declarations: [LoggingInComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginModule { }
