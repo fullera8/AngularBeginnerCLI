63e10e5322dea8bd4b14853cf2e8961723ff70c8
Alex Fuller <fulleralex47@gmail.com>
2021-04-04 08:32:39 -0400

    adjusted user component do demo only partail test.

--- angular-rounting/src/app/admin/users/users.component.ts
+++ angular-rounting/src/app/admin/users/users.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  //user vars for testing
+  users: any[] = [];
+  hasPermission: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.hasPermission) {
+      this.getUsers()
+        .then(users => this.users = users)
+        .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+  async getUsers() {
+    return [
+      {name: 'Alex', email: 'alex@angular.com'},
+      {name: 'John', email: 'john@angular.com'}
+    ];
   }
 
 }
