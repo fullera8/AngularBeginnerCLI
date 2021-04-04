13cb515e4cbb3ebde66781844cdd0a1057629c72
Alex Fuller <fulleralex47@gmail.com>
2021-04-04 09:44:33 -0400

    Added in nested library project and referenced it in the app.

---
+++ angular-rounting/projects/my-logger/src/lib/my-logger.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MyLoggerComponent } from './my-logger.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: []
+})
+export class MyLoggerModule { }
--- angular-rounting/src/app/app.component.ts
+++ angular-rounting/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggerService } from 'my-logger';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-rounting';
+
+  constructor(private loggerService: LoggerService) {
+    loggerService.log('hello world!');
+  }
 }
