b92a6a9d0a26de074aae410a7696dd49ffaa58ff
Alex Fuller <fulleralex47@gmail.com>
2021-04-04 08:15:37 -0400

    Update title test

--- angular-rounting/src/app/app.component.spec.ts
+++ angular-rounting/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-rounting app is running!');
+    expect(compiled.querySelector('.content h2').textContent).toContain('angular-rounting');
   });
 });
